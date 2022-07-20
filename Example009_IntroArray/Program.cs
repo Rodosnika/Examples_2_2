a3e372e9b5cba650d6bda890c5d00a738aa08d12
Veronika <vvnika@mail.ru>
2022-07-20 21:59:21 +1100

    add "Example009_IntroArray, определение max через массив" .....   Max(array[0], array[1], array[2])....
       Console.WriteLine(max);

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -8,9 +8,7 @@
 // т.е. если 9элементов индек начинается с 0 до 8
 //              0   1   2   3   4   5   6   7   8
 int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
-array[0] = 12;
 
-Console.WriteLine(array[4]);
 
 //int max1 = Max (a1,b1,c1);
 //int max2 = Max (a2,b2,c2);
@@ -23,3 +21,9 @@ int max = Max(
 
 Console.WriteLine(max);
 */
+int max = Max(
+   Max(array[0], array[1], array[2]),
+   Max(array[3], array[4], array[5]),
+   Max(array[6], array[7], array[8]));
+   
+   Console.WriteLine(max);
