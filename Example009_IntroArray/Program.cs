36fccae6b192e4a4df9b3b135a4aa46aca59fb72
Veronika <vvnika@mail.ru>
2022-07-20 21:29:25 +1100

    add "Поиск max через array [0] -обращение к массиву"

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,25 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+} // 0 1 2 3 4 5 6 7 8 индексы массива технически начинаются с 0 
+// т.е. если 9элементов индек начинается с 0 до 8
+//              0   1   2   3   4   5   6   7   8
+int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
+array[0] = 12;
+
+Console.WriteLine(array[4]);
+
+//int max1 = Max (a1,b1,c1);
+//int max2 = Max (a2,b2,c2);
+//int max3 = Max (a3,b3,c3);
+/*
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+
+Console.WriteLine(max);
+*/
